c2fd518889a4bfb2089e6cdd4429f10705e97e58
mdefitko777 <mdefitko@gmail.com>
2025-03-20 11:51:45 +0900

    m

--- github.py
+++ github.py
@@ -1,9 +1,6 @@
 print("github")
-<<<<<<< HEAD
 print('11111')
-=======
 print("정박문 바보")
 for i in range (5):
   print("박연우 천재")
->>>>>>> 6ebf91bcdfcf0dd419d3598cc4a081c4d1290aec
-wwww
+print('이게머냐')
